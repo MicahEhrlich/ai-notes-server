d19891df80377ba211acab20c65963f8a32f31c6
Micah Ehrlich <micahehrlich@gmail.com>
2025-05-19 23:45:50 +0300

    added user_id + username in token post request

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 def on_startup():
     SQLModel.metadata.create_all(engine)
@@ -108,7 +109,8 @@ def login(user: UserLogin, session: Session = Depends(get_session)):
 
     access_token = create_access_token(data={"sub": str(user_row.id)},
                                        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    return {"access_token": access_token, "token_type": "bearer"}
+    user = get_current_user(access_token)
+    return {"access_token": access_token, "token_type": "bearer", "user_id": user.id, "username": user.username}
 
 
 # --- Notes Endpoints ---
